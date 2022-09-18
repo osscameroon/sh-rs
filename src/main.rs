bbd1984f156b23ebc2695af987fbc2d5ba7f3727
Paul Mairo <herrmpr@gmail.com>
2022-09-18 09:16:43 +0200

    Implement the execution of a command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     loop {
@@ -6,5 +7,9 @@ fn main() {
         print!("> ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).expect("Error reading user input");
+        let args: Vec<&str> = command[..command.len()-1].split(" ").collect();
+        let output = Command::new(args[0]).args(&args[1..]).output().expect("Command execution failed");
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
     }
 }
