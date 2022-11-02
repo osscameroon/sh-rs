30cc5b70087b5d6fd321079887323120f97e29c7
Fomegne <fomegnemeudje@outlook.com>
2022-11-02 19:15:45 +0100

    pipe and operator

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,123 @@
 use std::io::{self, Write};
 use std::process::Command;
 
+#[derive(Debug)]
+struct Token {
+    cmd: Option<String>,
+    params: Vec<String>,
+    status: Option<bool>,
+    on_read: Option<Box<Token>>,
+    on_success: Option<Box<Token>>,
+    on_failure: Option<Box<Token>>,
+    next: Option<Box<Token>>
+}
+
+impl Token {
+    fn new() -> Self {
+        Self {
+            cmd: None,
+            params: Vec::new(),
+            status: None,
+            on_read: None,
+            on_success: None,
+            on_failure: None,
+            next: None
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Cursor {
+    word: String,
+    prev_char: char
+}
+
+impl Cursor {
+    fn new() -> Self {
+        Self {
+            word: String::new(),
+            prev_char: '\0'
+        }
+    }
+}
+
+fn parse(s: &str) {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut curr_token = Token::new();
+    let mut cursor = Cursor::new();
+
+    for c in s.chars() {
+        match c {
+            ' ' | ';' => {
+                match cursor.prev_char {
+                    '|' | '&' | ' ' | ';' | '\0' => { },
+                    _ => {
+                        if cursor.word == "" {
+                            // Do nothing!
+                        } else if curr_token.cmd == None {
+                            curr_token.cmd = Some(cursor.word);
+                        } else {
+                            curr_token.params.push(cursor.word);
+                        }
+                        if c == ';' {
+                            tokens.push(curr_token);
+                            curr_token = Token::new();
+                        }
+                    },
+                };
+                cursor.word = String::new();
+            },
+            '|' => {
+                match cursor.prev_char {
+                    '|' => {
+                        curr_token.on_failure = curr_token.on_read;
+                        curr_token.on_read = None;
+                    },
+                    _ => {
+                        let prev_token = curr_token;
+                        curr_token = Token::new();
+                        curr_token.on_read = Some(Box::new(prev_token));
+                    }
+                };
+                cursor.word = String::new();
+            },
+            '&' => {
+                match cursor.prev_char {
+                    '&' => {
+                        curr_token.on_success = curr_token.next;
+                        curr_token.next = None;
+                    },
+                    _ => {
+                        let prev_token = curr_token;
+                        curr_token = Token::new();
+                        curr_token.next = Some(Box::new(prev_token));
+                    }
+                };
+                cursor.word = String::new();
+            },
+            '\n' => {
+                tokens.push(curr_token);
+                // We suppose end of the commandline
+                break;
+            },
+            _ => {
+                cursor.word.push(c);
+            }
+        }
+        println!("{:#?}", cursor);
+        cursor.prev_char = c;
+    }
+
+    println!("{:#?}", tokens);
+}
+
 fn main() {
     loop {
         let mut command = String::new();
-        let pwd = Command::new("pwd").output().expect("Command execution failed");
+        let pwd = Command::new("pwd").output().expect("Getting of the current dir failed");
         print!("{}> ", String::from_utf8_lossy(&pwd.stdout[..pwd.stdout.len()-1]));
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).expect("Error reading user input");
-        let args: Vec<&str> = command[..command.len()-1].split(" ").collect();
-        let output = Command::new(args[0]).args(&args[1..]).output().expect("Command execution failed");
-        io::stdout().write_all(&output.stdout).unwrap();
-        io::stderr().write_all(&output.stderr).unwrap();
+        parse(&command);
     }
 }
