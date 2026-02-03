7fc9d836829b4e09716ec411d009c36e115f52f4
Paul Mairo <herrmpr@gmail.com>
2026-02-03 11:35:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ fn main() {
     let mut stdin = io::stdin();
     let input = &mut String::new();
     loop {
+        print!("$ ");
         input.clear();
-        stdin.read_line(input);
-        println!("{}: command not found", input);
+        stdin.read_line(input).unwrap();
+        let command = input.trim();
+        if !command.is_empty() {
+            println!("{}: command not found", input);
+        }
         io::stdout().flush().unwrap();
     }
 }
