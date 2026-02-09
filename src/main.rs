47d7cfaa0fa6e0fbb9b04615f89b72b026671655
Paul Mairo <herrmpr@gmail.com>
2026-02-09 09:53:52 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process::exit;
+use std::error::Error;
 
-fn main() {
+fn parse_command(command: &str) -> Vec<String> {
+    if command == "exit" {
+        exit(0);
+    }
+    let commands: Vec<String> = command.split(' ').map(String::from).collect();
+    commands
+}
+
+fn execute_command(tokens: Vec<String>) {
+    if tokens[0] == "echo" {
+        for token in &tokens[1..] {
+            print!("{} ", token);
+        }
+    }
+    let command = tokens.into_iter().collect::<String>();
+    println!("{}: command not found", command);
+}
+fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
     let input = &mut String::new();
     loop {
@@ -11,11 +29,9 @@ fn main() {
         input.clear();
         stdin.read_line(input).unwrap();
         let command = input.trim();
-        if command == "exit" {
-            exit(0);
-        }
         if !command.is_empty() {
-            println!("{}: command not found", command);
+            let parsed_command = parse_command(command);
+            execute_command(parsed_command);
         }
         io::stdout().flush().unwrap();
     }
